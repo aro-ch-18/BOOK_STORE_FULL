61ea743445889fdfda2319ac4e0e2b46864de6d6
Arohan <arohangurung180604@gmail.com>
2024-08-07 01:20:23 +0530

    commit-nav-flex

--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -29,7 +29,7 @@ const Navbar = ({ isloggedIn, setIsLoggedIn }) => {
   };
 
   return (
-    <nav className="bg-gray-800 p-4 min-h-[53px] h-[8vh]  ">
+    <nav className="bg-gray-800 p-4 min-h-[53px] h-[8vh]  flex items-center justify-center">
       <div className="container mx-auto flex justify-between items-center">
         {/* Avatar and greeting */}
         <div
